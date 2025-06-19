03e2c3d7c86e7ff40c33147bde8199814f3177b5
Sam <samroux808@gmail.com>
2025-06-19 11:28:52 -0500

    Updated algorithm weights and added function compareArrays

--- src/libs/confidence.js
+++ src/libs/confidence.js
@@ -2,6 +2,31 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.calculateConfidence = void 0;
 const tlsh_1 = require("./tlsh");
+function compareArrays(arr1, arr2) {
+    let fields = 0;
+    let matches = 0;
+    const maxLength = Math.max(arr1.length, arr2.length);
+    for (let i = 0; i < maxLength; i++) {
+        if (arr1[i] !== undefined && arr2[i] !== undefined) {
+            fields++;
+            if (Array.isArray(arr1[i]) && Array.isArray(arr2[i])) {
+                const subData = compareArrays(arr1[i], arr2[i]);
+                fields += subData[0] - 1; // Subtract 1 for the index itself
+                matches += subData[1];
+            }
+            else if ((typeof arr1[i] == "object" && arr1[i]) &&
+                (typeof arr2[i] == "object" && arr2[i])) {
+                const subData = compareDatasets(arr1[i], arr2[i]);
+                fields += subData[0] - 1; // Subtract 1 for the index itself
+                matches += subData[1];
+            }
+            else if (arr1[i] === arr2[i]) {
+                matches++;
+            }
+        }
+    }
+    return [fields, matches];
+}
 function compareDatasets(data1, data2) {
     let fields = 0;
     let matches = 0;
@@ -11,6 +36,11 @@ function compareDatasets(data1, data2) {
             if ((typeof data1[key] == "object" && data1[key]) &&
                 (typeof data2[key] == "object" && data2[key])) {
                 const subData = compareDatasets(data1[key], data2[key]);
+                fields += subData[0] - 1; // Subtract 1 for the key itself
+                matches += subData[1];
+            }
+            else if (Array.isArray(data1[key]) && Array.isArray(data2[key])) {
+                const subData = compareArrays(data1[key], data2[key]);
                 fields += subData[0] - 1; // Subtract 1 for the key itself
                 matches += subData[1];
             }
@@ -33,11 +63,11 @@ function calculateConfidence(data1, data2) {
     // Compare the hashes to get their difference
     const differenceScore = (0, tlsh_1.compareHashes)(hash1, hash2);
     const inverseMatchScore = 1 - (matches / fields);
-    const x = differenceScore * inverseMatchScore;
+    const x = 1.3 * differenceScore * inverseMatchScore;
     if (inverseMatchScore === 0 || differenceScore === 0) {
         return 100;
     }
-    const confidenceScore = 100 / (1 + Math.E ** (-4.5 + (0.25 * x)));
+    const confidenceScore = 100 / (1 + Math.E ** (-4.5 + (0.3 * x)));
     return confidenceScore;
 }
 exports.calculateConfidence = calculateConfidence;
--- src/libs/confidence.ts
+++ src/libs/confidence.ts
@@ -1,6 +1,32 @@
 import { getHash, compareHashes } from "./tlsh";
 import { FPDataSet } from "../types/data";
 
+function compareArrays(arr1: any[], arr2: any[]): [number, number] {
+  let fields = 0;
+  let matches = 0;
+  const maxLength = Math.max(arr1.length, arr2.length);
+  for (let i = 0; i < maxLength; i++) {
+    if (arr1[i] !== undefined && arr2[i] !== undefined) {
+      fields++;
+      if (Array.isArray(arr1[i]) && Array.isArray(arr2[i])) {
+        const subData = compareArrays(arr1[i], arr2[i]);
+        fields += subData[0] - 1; // Subtract 1 for the index itself
+        matches += subData[1];
+      } else if (
+        (typeof arr1[i] == "object" && arr1[i]) &&
+        (typeof arr2[i] == "object" && arr2[i])
+      ) {
+        const subData = compareDatasets(arr1[i] as FPDataSet, arr2[i] as FPDataSet);
+        fields += subData[0] - 1; // Subtract 1 for the index itself
+        matches += subData[1];
+      } else if (arr1[i] === arr2[i]) {
+        matches++;
+      }
+    }
+  }
+  return [fields, matches];
+}
+
 function compareDatasets(data1: FPDataSet, data2: FPDataSet): [number, number] {
   let fields = 0;
   let matches = 0;
@@ -15,6 +41,13 @@ function compareDatasets(data1: FPDataSet, data2: FPDataSet): [number, number] {
         fields += subData[0] - 1; // Subtract 1 for the key itself
         matches += subData[1];
       }
+
+      else if (Array.isArray(data1[key]) && Array.isArray(data2[key])) {
+        const subData = compareArrays(data1[key], data2[key]);
+        fields += subData[0] - 1; // Subtract 1 for the key itself
+        matches += subData[1];
+      }
+
       if (data1[key] == data2[key]) {
         matches++;
       }
@@ -39,10 +72,10 @@ export function calculateConfidence(data1: FPDataSet, data2: FPDataSet): number 
   const differenceScore = compareHashes(hash1, hash2);
 
   const inverseMatchScore = 1 - (matches / fields);
-  const x = differenceScore * inverseMatchScore
+  const x = 1.3 * differenceScore * inverseMatchScore
   if (inverseMatchScore === 0 || differenceScore === 0) {
     return 100;
   }
-  const confidenceScore = 100 / (1 + Math.E ** (-4.5 + (0.25 * x)));
+  const confidenceScore = 100 / (1 + Math.E ** (-4.5 + (0.3 * x)));
   return confidenceScore;
 }
